a64b90a46fe019f23f74542d01db8e82565c2939
José Antonio Céspedes Downing <tonio782009@hotmail.com>
2018-09-12 23:40:08 -0600

    Done

--- src/encode/MatrixToJson.java
+++ src/encode/MatrixToJson.java
@@ -1,3 +1,4 @@
+
 package encode;
 
 import linkedlist.LinkedList;
@@ -6,12 +7,21 @@ import matrix.Matrix;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Translates a Matrix instance to/from .json format.
  *
  * @author David Azofeifa H.
-*/
-public class matrixToJson {
+ */
+public class MatrixToJson {
 
     /**
      * Encodes a Matrix instance into a JSONObject instance.
@@ -35,7 +45,7 @@ public class matrixToJson {
             JSONArray temp = new JSONArray();
             Node tempNode2 = ((LinkedList) tempNode1.getData()).getHead();
             for (int j=0; j < matrix.getColumns(); j++) {
-                temp.put(tempNode2.getData());
+               temp.put(tempNode2.getData());
                 tempNode2 = tempNode2.getNext();
             }
             _matrix.put(temp);
@@ -49,8 +59,8 @@ public class matrixToJson {
      * Decodes a JSONObject instance into a Matrix Instance.
      *
      * @param jsonDoc, JSONObject instance
-     * @return Matrix instance
-     */
+    * @return Matrix instance
+    */
     public Matrix decodeMatrix(JSONObject jsonDoc) {
         //TODO: improve
         int scorePlayer1 = 0;
@@ -62,40 +72,48 @@ public class matrixToJson {
         scorePlayer2 = score.getInt(1);
 
         JSONArray _matrix = (JSONArray) jsonDoc.get("matrix");
-        
-        JSONArray
 
-        Matrix tempMatrix = new Matrix(_matrix.length(), _);
+        int matrixColumns = _matrix.length();
+        int matrixRows = ((JSONArray) _matrix.get(0)).length();
+
+        // TODO: parametrizar inicial value
+        Matrix matrix = new Matrix(matrixRows, matrixColumns, 0);
+
+        for (int j = 0; j < matrixColumns; j++) {
+            JSONArray tempColumn = (JSONArray) _matrix.get(j);
+            for (int i = 0; i < matrixRows; i++) {
+                int tempValue = tempColumn.getInt(i);
+                matrix.changeValue(i, j, tempValue);
+            }
+        }
 
         return matrix;
+    }
+
+    public void saveJsonFile(JSONObject jsonDoc) {
+        List<String> lines = Arrays.asList(jsonDoc.toString(4));
+        Path file = Paths.get("matrixAsJson.json");
+        try {
+            Files.write(file, lines, Charset.forName("UTF-8"));
+        }
+        catch (IOException e) {
+            System.out.println(e.toString());
+            System.out.println("Coudn't save fila in specified path");
+        }
     }
 
     public void fetchJsonFile() {
 
     }
 
-
     public static void main(String[] args) {
-
-        JSONObject general = new JSONObject();
-        JSONObject temp = new JSONObject();
-
-        temp.put("test", 0);
-
-        general.put("rows", 5);
-        general.put("columnas", 5);
-        general.put("myTestie", temp);
-
-        JSONArray array = new JSONArray();
-
-        array.put(3);
-        array.put(4);
-
-
-        general.put("puntos",array);
-
-        System.out.println(general.keySet());
-
+        MatrixToJson conv = new MatrixToJson();
+        Matrix matrix = new Matrix(4,6,1);
+        LinkedList score = new LinkedList();
+        score.append(300);
+        score.append(900);
+        JSONObject tempMatrixJson = conv.encondeMatrix(matrix, score);
+        conv.saveJsonFile(tempMatrixJson);
     }
 
 }
