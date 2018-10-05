76113a67ecec1ff0a4024ea90da54d69cb25edf9
José Antonio Céspedes Downing <tonio782009@hotmail.com>
2018-10-05 04:23:25 -0600

    Cambios en el cliente

--- src/comm/Client.java
+++ src/comm/Client.java
@@ -41,95 +41,25 @@ public class Client extends Thread{
         this.game = new Game(this.rows, this.columns, this.initialValue);
     }
 
-
-
     public void start() {
-    	ClientThread clithr = new ClientThread(socket);
+        ClientThread clthr = new ClientThread(socket);
         String name = game.startMenu();
         Parse parser = new Parse();
         JSONObject nameDoc = parser.namaAsJson(name);
         Conversion conv = new Conversion();
         conv.saveJsonFile(nameDoc, "name.json");
         try {
-            clithr.sendFirstJson(nameDoc);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-			listen(clithr);
-		} catch (IOException | ParseException e) {
-			// TODO Auto-generated catch block
+			clthr.sendFirstJson(nameDoc);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+        
     }
     
-    public void listen(ClientThread clithr) throws IOException, ParseException {
-    	while(true) {
-    		JSONObject nombres = clithr.receiveFirstJson();
-    		System.out.println(nombres.toString());
-    	}
-    	
-    }
-    
-    /**
-     * Reads incoming file and attempts to read it and form a JSONObject instance.
-     *
-     * @throws IOException
-     * @throws ParseException
-     */
-    private void receiveJson() throws IOException, ParseException{
-        BufferedReader brs = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        try{
-            String JsonString = brs.readLine();
-            System.out.println(JsonString);
-            JSONParser parserS = new JSONParser();
-            JSONObject json = (JSONObject) parserS.parse(new InputStreamReader(new FileInputStream("gameState.json"))); //de String a Json
-            System.out.println(JsonString);
-            Parse parserM = new Parse();
-
-            JSONObject obj = game.getGameState(pMouse);
-            Parse parser = new Parse();
-//              LinkedList list = parser.JsonToGameState(obj);
-//              Matrix matrix = (Matrix) list.getHead().getData();
-//              matrix.printMatrix();
-//            
-            LinkedList list = parserM.JsonToGameState(obj);
-            game.updateGameState(list);
-
-        }
-        catch (UnknownHostException ex) {
-            System.out.println("Server not found: " + ex.getMessage());
-        }
-        catch (IOException ex) {
-            System.out.println("I/O error: " + ex.getMessage());
-        }
-        this.sendJson();
-    }
-
-    /**
-     * Sends .json file to the server.
-     * @throws ParseException
-     * @throws IOException
-     */
-    private void sendJson() throws IOException, ParseException {
-
-        JSONObject obj = game.getGameState(pMouse);
-
-        try {
-            PrintStream ps = new PrintStream(socket.getOutputStream());
-            ps.println("Client: " + obj);
-            // socket.close();
-        } catch (IOException ex) {
-            System.out.println("Server exception: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-        this.receiveJson();
-    }
 
     public static void main(String[] args) throws Exception {
         Client client = new Client("192.168.100.24", 4444);
         client.start();
-    }
+     }
 
 }
--- src/comm/ClientThread.java
+++ src/comm/ClientThread.java
@@ -1,15 +1,21 @@
 package comm;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import json_parse.Parse;
+import linkedlist.LinkedList;
 
 public class ClientThread extends Thread{
 	private Socket socket;
@@ -27,16 +33,88 @@ public class ClientThread extends Thread{
     public JSONObject receiveFirstJson() throws IOException, ParseException {
         BufferedReader brs = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         JSONObject obj = new JSONObject();
-        try{
-            String nombres = brs.readLine();
-            JSONParser parser1 = new JSONParser();
-            JSONObject jsonConNombres = (JSONObject) parser1.parse(nombres);
-            obj = jsonConNombres;
-        }catch(IOException ex){
-            System.out.println("I/O error: "+ ex.getMessage());
+        while(brs.readLine() != null) {
+        	try{
+                String nombres = brs.readLine();
+                JSONParser parser1 = new JSONParser();
+                JSONObject jsonConNombres = (JSONObject) parser1.parse(nombres);
+                obj = jsonConNombres;
+            }catch(IOException ex){
+                System.out.println("I/O error: "+ ex.getMessage());
+            }
         }
-		return obj;
+        return obj;
     }
 	
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /**
+     * Reads incoming file and attempts to read it and form a JSONObject instance.
+     *
+     * @throws IOException
+     * @throws ParseException
+    
+    private void receiveJson() throws IOException, ParseException{
+        BufferedReader brs = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try{
+            String JsonString = brs.readLine();
+            System.out.println(JsonString);
+            JSONParser parserS = new JSONParser();
+            JSONObject json = (JSONObject) parserS.parse(new InputStreamReader(new FileInputStream("gameState.json"))); //de String a Json
+            System.out.println(JsonString);
+            Parse parserM = new Parse();
+
+            JSONObject obj = game.getGameState(pMouse);
+            Parse parser = new Parse();
+//              LinkedList list = parser.JsonToGameState(obj);
+//              Matrix matrix = (Matrix) list.getHead().getData();
+//              matrix.printMatrix();
+//            
+            LinkedList list = parserM.JsonToGameState(obj);
+            game.updateGameState(list);
+
+        }
+        catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        }
+        catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+        this.sendJson();
+    }
+    **/
+
+    /**
+     * Sends .json file to the server.
+     * @throws ParseException
+     * @throws IOException
+     
+    private void sendJson() throws IOException, ParseException {
+
+        JSONObject obj = game.getGameState(pMouse);
+
+        try {
+            PrintStream ps = new PrintStream(socket.getOutputStream());
+            ps.println("Client: " + obj);
+            // socket.close();
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+        this.receiveJson();
+    }
 	
+    */
 }
